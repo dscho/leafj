9478c8c966fde5949305b991eb301f2cb26a0c71
Julin Maloof <jnmaloof@ucdavis.edu>
2012-04-09 21:12:17 -0700

    Add row and add column buttons working in rudimentary fashion. done button prints contents to console

--- sandbox/practiceTable.java
+++ sandbox/practiceTable.java
@@ -3,9 +3,13 @@ package sandbox;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
@@ -15,6 +19,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowListener;
+import java.util.Vector;
 
 
 
@@ -48,6 +53,14 @@ public class practiceTable{
 		JButton cancelButton = new JButton("Cancel");
 		cancelButton.addActionListener(new cancelListener());
 		
+		
+		JTextArea info = new JTextArea("Each column represents one type of annotation\n" + 
+										"place the name for the data type in the first row\n" +
+										"if you want to have a pull-down list of allowable data\n" +
+										"then place allowable values in subsequent rows\n" +
+										"otherwise leave blank and a text entry field will be presented\n");
+		info.setEditable(false);
+		buttonPanel.add(info);
 		buttonPanel.add(addRowButton);
 		buttonPanel.add(addColumnButton);
 		buttonPanel.add(doneButton);
@@ -106,18 +119,30 @@ public class practiceTable{
 	class addRowListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			System.out.println("row listener");
+			DefaultTableModel tmodel = (DefaultTableModel) table.getModel();
+			tmodel.addRow(new Vector<Object>());
 		}
 	}
 
 	class addColListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			System.out.println("col listener");
+			TableColumn tc = new TableColumn();
+			tc.setHeaderValue("new column");
+			table.addColumn(tc);
 		}
 	}
 	
 	class doneListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			System.out.println("Done listener");
+		frame.setVisible(false);
+		for(int col = 0; col < table.getColumnCount();col++) {
+			for(int row = 0; row < table.getRowCount();row++) {
+				System.out.println(table.getValueAt(row, col));
+			}
+		}
+		frame.dispose();
 		}
 	}
 	
